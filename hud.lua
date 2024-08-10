b9215acee484b9912b54a07a2cee8cc213dd0e9b
zachshattuck <zdogshattuck@gmail.com>
2024-08-10 00:51:59 -0600

    attempt to display rows for levels, has errors

--- hud.lua
+++ hud.lua
@@ -2,19 +2,51 @@ local screenWidth = djui_hud_get_screen_width()
 local screenHeight = djui_hud_get_screen_height()
 
 local timer = 0
+local per_page = 5
+
+local row_width = screenWidth*3/4
+local row_height = 30
+
+local function cleanup_hud()
+  gGlobalSyncTable.lpShowHud = false
+  gGlobalSyncTable.lpPage = 1
+end
 
 local function hud_render()
-  if gGlobalSyncTable.showHud == false then
+  --- @type table | nil
+  if gGlobalSyncTable.lpLevels == nil then
+    return
+  end
+  if gGlobalSyncTable.lpShowHud == false then
     return
   end
 
   djui_hud_set_color(0, 0, 0, 255)
   djui_hud_render_rect(0, 0, screenWidth, screenHeight)
 
-  -- djui_hud_print_text("Hello from levelpicker!", 100, 100, 1)
   local text = "Hello from levelpicker!"
   djui_hud_set_color(100, 255, 100, 255)
   djui_hud_print_text(text, (screenWidth - djui_hud_measure_text(text)) / 2, 50, 1)
+
+  for i = gGlobalSyncTable.lpPage, gGlobalSyncTable.lpPage+per_page, 1 do
+    local level_idx = gGlobalSyncTable*per_page + i
+    local row_x = screenWidth / 2 - row_width - 2
+    local row_y = 50 + (row_height + 2) * 1
+
+    djui_hud_set_color(100, 100, 100, 255)
+    djui_hud_render_rect(row_x, row_y, row_width, row_height)
+
+    local level = gGlobalSyncTable.lpLevels[level_idx]
+    if level == nil then
+      djui_hud_set_color(255, 0, 0, 255)
+      djui_hud_print_text("uh oh", 0, 0, 1)
+      return
+    end
+    local message = level.fullName .. " (" .. level.shortName .. ")"
+
+    djui_hud_set_color(100, 255, 100, 255)
+    djui_hud_render_rect(message, row_x, row_y, 1)
+  end
 
   -- Keep screen size variables up to date
   screenWidth = djui_hud_get_screen_width()
@@ -22,7 +54,7 @@ local function hud_render()
 
   timer = timer + 1
   if timer == 60*5 then
-    gGlobalSyncTable.showHud = false
+    gGlobalSyncTable.lpShowHud = false
   end
 end
 
--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 -- name: Level Picker
 -- description: Easily view and warp to all levels, modded or vanilla.
 
-gGlobalSyncTable.showHud = false
+gGlobalSyncTable.lpShowHud = false
+gGlobalSyncTable.lpPage = 1
+
+---@type table | nil
+-- Array of levels, or `nil` if `scan_levels` has not been called yet.
+gGlobalSyncTable.lpLevels = nil
 
 -- Max possible level number (`u16`).  
 -- *(Based on `CustomLevelInfo` in `smlua_level_utils.h`)*
@@ -12,71 +17,72 @@ local COLOR_DEFAULT = "\\#FFFFFF\\"
 local COLOR_YELLOW = "\\#CCCC00\\"
 local COLOR_AQUA = "\\#88EEAA\\"
 
--- Array of levels, or `nil` if `scan_levels` has not been called yet.
----@type table | nil
-local levels = nil
 
 local function scan_levels()
   local levels = {}
+  local count = 0
   for i = 1, MAX_LEVEL_NUMBER, 1 do
     local level = smlua_level_util_get_info(i)
     if level then
       table.insert(levels, level)
+      count = count+1
     end
   end
-  return levels
+  -- return levels
+  gGlobalSyncTable.lpLevels = levels
+  gGlobalSyncTable.levelsCount = count
 end
 
-local function list_levels()
-  if levels == nil then
-    return
-  end
+-- local function list_levels()
+--   if levels == nil then
+--     return
+--   end
 
-  ---@param level CustomLevelInfo
-  ---@param idx integer
-  for idx,level in pairs(levels) do
-    local message = string.format(
-      COLOR_AQUA .. "%d: " .. COLOR_DEFAULT .. level.fullName .. " (" .. level.shortName .. ")",
-      idx
-    )
+--   ---@param level CustomLevelInfo
+--   ---@param idx integer
+--   for idx,level in pairs(levels) do
+--     local message = string.format(
+--       COLOR_AQUA .. "%d: " .. COLOR_DEFAULT .. level.fullName .. " (" .. level.shortName .. ")",
+--       idx
+--     )
 
-    djui_chat_message_create(message)
-  end
+--     djui_chat_message_create(message)
+--   end
 
-  djui_chat_message_create(COLOR_YELLOW .. "NOTE: If a level listed more than once, it probably exists for multiple game modes." .. COLOR_DEFAULT)
-end
+--   djui_chat_message_create(COLOR_YELLOW .. "NOTE: If a level listed more than once, it probably exists for multiple game modes." .. COLOR_DEFAULT)
+-- end
 
----@param num string
-local function try_warp_to_level(num)
-  if levels == nil then
-    return
-  end
+-- ---@param num string
+-- local function try_warp_to_level(num)
+--   if levels == nil then
+--     return
+--   end
 
-  -- Attempt to parse level number    
-  local levelNum = tonumber(num)
-  if levelNum == nil then
-    djui_chat_message_create(COLOR_ERROR .. "Invalid level number." .. COLOR_DEFAULT)
-    return
-  end
+--   -- Attempt to parse level number    
+--   local levelNum = tonumber(num)
+--   if levelNum == nil then
+--     djui_chat_message_create(COLOR_ERROR .. "Invalid level number." .. COLOR_DEFAULT)
+--     return
+--   end
 
-  --- @type CustomLevelInfo | nil
-  local level = levels[levelNum]
-  if level == nil then
-    djui_chat_message_create(COLOR_ERROR .. "Unknown level." .. COLOR_DEFAULT)
-    return
-  end
+--   --- @type CustomLevelInfo | nil
+--   local level = levels[levelNum]
+--   if level == nil then
+--     djui_chat_message_create(COLOR_ERROR .. "Unknown level." .. COLOR_DEFAULT)
+--     return
+--   end
 
-  djui_chat_message_create(COLOR_AQUA .. "Warping to \"" .. level.fullName .. "\"..." .. COLOR_DEFAULT)
-  warp_to_level(level.levelNum, 1, 0)
-end
+--   djui_chat_message_create(COLOR_AQUA .. "Warping to \"" .. level.fullName .. "\"..." .. COLOR_DEFAULT)
+--   warp_to_level(level.levelNum, 1, 0)
+-- end
 
 -- TODO: Match on shortName and fullName too
 hook_chat_command("lp", "({number}) List and teleport to available levels", function (msg)
-  if levels == nil then
-    levels = scan_levels()
+  if gGlobalSyncTable.lpLevels == nil then
+    scan_levels()
   end
 
-  gGlobalSyncTable.showHud = true
+  gGlobalSyncTable.lpShowHud = true
   return true
 
   -- if msg:len() > 0 then
